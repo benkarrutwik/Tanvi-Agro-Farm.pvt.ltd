8ef25f5438517dc0647797a34977b69012c91fe9
benkarrutwik <benkarrutwik29@gmail.com>
2026-03-11 15:14:33 +0530

    .

---
+++ src/services/logoService.ts
@@ -0,0 +1,11 @@
+import { GoogleGenAI } from "@google/genai";
+
+export const generateOrganicLogo = async () => {
+  // User provided static logo for 100% Organic
+  return 'https://i.ibb.co/1J9th42V/9106212-26205.jpg';
+};
+
+export const generateNaturalLogo = async () => {
+  // User provided static logo for Natural & Ethical
+  return 'https://i.ibb.co/ccnD0HLk/people-caring-protecting-mother-earth-earth-day.jpg';
+};
